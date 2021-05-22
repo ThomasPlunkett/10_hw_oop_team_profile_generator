ecb6f6874675cd896962dc3e6ffea077010952c9
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-22 16:55:35 -0400

    installed npm packages for consolelog testing. modified src indexjs to create prompts and try to get back some answers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 // BECAUSE I'M PUTTING page-template.js IN ANOTHER FOLDER (src),  
 // REQUIRE that page-template.js
+const inquirer = require('inquirer');
 const pageTemplate = require('./src/page-template.js');
 // USE pageTemplate as a function, which can ACCEPT a parameter
 pageTemplate(team);
@@ -13,3 +14,30 @@ pageTemplate(team);
 // FS WRITEFILE
 
 // OUTPUT HTML FILES WILL LAND IN DIST FOLDER
+function runApp() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is your team manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the team manager's id?",
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email?",
+        },
+        {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: "What is your team manager's office number?",
+        },
+    ]).then(answers => {
+        console.log(answers);
+    })
+    
+}
