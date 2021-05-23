cfc407820126f5932dc50e02aa078615856080cb
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-23 14:38:03 -0400

    modified page-template.js need to generate the html pages. pseudocoding methods to render info

--- src/page-template.js
+++ src/page-template.js
@@ -1,1 +1,8 @@
 // GENERATE THE HTML PAGES
+const generateTeam  = team => {
+    // a method for a template to render manager info
+
+    // a method for a template to render engineer info
+
+    // a method for a template to render intern info
+}
