bb7ad6dd60432770bc9868b680d8b1f7e9c0f53c
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-23 15:10:36 -0400

    added html hard code to function on page-template.js commented out psuedocode for testing

--- src/page-template.js
+++ src/page-template.js
@@ -5,9 +5,9 @@ const generateTeam  = team => {
     // a method for a template to render engineer info
 
     // a method for a template to render intern info
-const generateManager = manager = {
-    return `${manager.getName()} etc etc`
-}    
+// const generateManager = manager = {
+//     return `${manager.getName()} etc etc`
+// }    
 const html = [];
     html.push(team
         .filter(employee => employee.getRole() === "Manager")
@@ -19,5 +19,32 @@ const html = [];
 // export out an anonymous function
 module.exports = team => {
     return `
-    
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=”stylesheet” href=”https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css”rel=”nofollow” integrity=”sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm” crossorigin=”anonymous”>
+<script src="https://kit.fontawesome.com/563b95b7bc.js" crossorigin="anonymous"></script>   
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-center">
+                YOUR METHOD GOES HERE THE CARDS OF MANAGER, INTERN, ENGINEER WILL BE.
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    `
 }
