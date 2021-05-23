b9008bb9c17c978f5fc0c60698f0944a56f880f0
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-23 14:47:48 -0400

    export anonymous function to return html

--- src/page-template.js
+++ src/page-template.js
@@ -5,4 +5,19 @@ const generateTeam  = team => {
     // a method for a template to render engineer info
 
     // a method for a template to render intern info
+const generateManager = manager = {
+    return `${manager.getName()} etc etc`
+}    
+const html = [];
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    return html.join("");
+}
+
+// export out an anonymous function
+module.exports = team => {
+    return `
+    
 }
