e2be8dcf7d9ff93e73c7c29df382e54b300d66e5
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-23 21:15:47 -0400

    making edits to repair errors logged while running node index.js in terminal

--- index.js
+++ index.js
@@ -1,177 +1,163 @@
-// POPULATE WITH MY MAIN LOGIC
-
-// REQUIRE ALL FILES AND NODE PACKAGES
-
-// BECAUSE I'M PUTTING page-template.js IN ANOTHER FOLDER (src),  
-// REQUIRE that page-template.js
 const inquirer = require('inquirer');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const render = require('./src/page-template.js');
+const fs = require('fs');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+const teamPageTemplate = require('./src/page-template'); // This is a function I can call and pass team into it
 const path = require("path");
-const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "dist");
+
+const OUTPUT_DIR = path.resolve(__dirname, "dist")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// USE pageTemplate as a function, which can ACCEPT a parameter
-pageTemplate(team);
+const team = [];
 
-// FS WRITEFILE
-function buildTeam() {
+
+function createTeamHTMLPage(htmlContent) {
     // Create the output directory if the output path doesn't exist
     if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR)
     }
-    fs.writeFileSync(outputPath, render(membersArray), "utf-8");
+    fs.writeFileSync(outputPath, htmlContent, "utf-8");
 }
-buildTeam();
 
-// OUTPUT HTML FILES WILL LAND IN DIST FOLDER
-const membersArray = [];
-// function to createManager
-function runApp() {
-    function createManager() {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'managerName',
-                message: "What is your team manager's name?",
-            },
-            {
-                type: 'input',
-                name: 'managerId',
-                message: "What is the team manager's id?",
-            },
-            {
-                type: 'input',
-                name: 'managerEmail',
-                message: "What is the team manager's email?",
-            },
-            {
-                type: 'input',
-                name: 'managerOfficeNumber',
-                message: "What is your team manager's office number?",
-            },
-            {
-                type: 'list',
-                name: 'chooseType',
-                message: 'Would you like to add an Engineer, an Intern, or end the program?',
-                choices: ['Add an Engineer', 'Add an Intern', 'End the program'],
-            }])
-            .then(answers => {
-            // I need to store this info based on manager, engineer, and intern
-            // so that I can call this information later on
-            // console.log(answers);
-            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNum);
-            // console.log(manager);
-            membersArray.push(manager);
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: "What is the intern's name?",
+        },
+        {
+            type: 'input',
+            name: 'internID',
+            message: "What is the intern's ID?",
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is the intern's Email?",
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "What is the intern's school name?",
+        },
+        {
+            type: 'list',
+            name: 'chooseType',
+            message: 'Would you like to add an Engineer, an Intern, or quit the program?',
+            choices: ['Add an Engineer', 'Add an Intern', 'Quit the Program'],
+        }])
+        // console.log("addEngineer was called");
+        .then((answers) => {
+            const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+
+            team.push(intern);
 
             if (answers.chooseType === "Add an Engineer") {
                 addEngineer();
             } else if (answers.chooseType === "Add an Intern") {
                 addIntern();
-            } else if (answers.chooseType === "End the Program") {
-                endProgram();
+            } else if (answers.chooseType === "Quit the Program") {
+                quitProgram();
             }
-            // console.log(membersArray);
         })
-    }
-    // function to createEngineer
-    function createEngineer() {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'engineerName',
-                message: "What is the engineer's name?",
-            },
-            {
-                type: 'input',
-                name: 'engineerId',
-                message: "What is the engineer's id?",
-            },
-            {
-                type: 'input',
-                name: 'engineerEmail',
-                message: "What is the engineer's email?",
-            },
-            {
-                type: 'input',
-                name: 'engineerGitHub',
-                message: "What is the engineer's GitHub username?",
-            },
-            {
-                type: 'list',
-                name: 'chooseType',
-                message: 'Would you like to add an Engineer, an Intern, or end the program?',
-                choices: ['Add an Engineer', 'Add an Intern', 'End the program'],
-            }])
-
-            .then((answers) => {
-                const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGitHub);
-    
-                membersArray.push(engineer);
-    
-                if (answers.chooseType === "Add an Engineer") {
-                    addEngineer();
-                } else if (answers.chooseType === "Add an Intern") {
-                    addIntern();
-                } else if (answers.chooseType === "End the Program") {
-                    endProgram();
-                }
-            })
-    }
-    // function to createIntern
-    function createIntern () {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'internName',
-                message: "What is the intern's name?",
-            },
-            {
-                type: 'input',
-                name: 'internID',
-                message: "What is the intern's Id?",
-            },
-            {
-                type: 'input',
-                name: 'internEmail',
-                message: "What is the intern's email?",
-            },
-            {
-                type: 'input',
-                name: 'internSchool',
-                message: "What is the intern's school name?",
-            },
-            {
-                type: 'list',
-                name: 'chooseType',
-                message: 'Would you like to add an Engineer, an Intern, or end the program?',
-                choices: ['Add an Engineer', 'Add an Intern', 'End the program'],
-            }])
-            
-            .then((answers) => {
-                const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
-    
-                membersArray.push(intern);
-    
-                if (answers.chooseType === "Add an Engineer") {
-                    addEngineer();
-                } else if (answers.chooseType === "Add an Intern") {
-                    addIntern();
-                } else if (answers.chooseType === "End the program") {
-                    endProgram();
-                }
-            })
-    }
-    // end program
-    const endProgram = () => {
-        let htmlContent = teamPageTemplate.htmlGenerator(team);
-        createTeamHTMLPage(htmlContent);
-    }
-        
 }
 
-runApp();
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is the engineer's name?",
+        },
+        {
+            type: 'input',
+            name: 'engineerID',
+            message: "What is the engineer's ID?",
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is the engineer's Email?",
+        },
+        {
+            type: 'input',
+            name: 'engineerGitHub',
+            message: "What is the engineer's GitHub username?",
+        },
+        {
+            type: 'list',
+            name: 'chooseType',
+            message: 'Would you like to add an Engineer, an Intern, or quit the program?',
+            choices: ['Add an Engineer', 'Add an Intern', 'Quit the Program'],
+        }])
+        // console.log("addEngineer was called");
+        .then((answers) => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGitHub);
+
+            team.push(engineer);
+
+            if (answers.chooseType === "Add an Engineer") {
+                addEngineer();
+            } else if (answers.chooseType === "Add an Intern") {
+                addIntern();
+            } else if (answers.chooseType === "Quit the Program") {
+                quitProgram();
+            }
+        })
+}
+
+const quitProgram = () => {
+    console.log(team);
+    let htmlContent = teamPageTemplate.htmlGenerator(team);
+    // console.log(htmlContent);
+    createTeamHTMLPage(htmlContent);
+    // Need to take the team array and pass that into a function to build the HTML based on a template - need this to return a string of HTML for the page.
+    console.log("\n");
+}
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'managerID',
+            message: "What is the team manager's ID?",
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's Email?",
+        },
+        {
+            type: 'input',
+            name: 'managerNumber',
+            message: "What is the team manager's office number?",
+        },
+        {
+            type: 'list',
+            name: 'chooseType',
+            message: 'Would you like to add an Engineer, an Intern, or quit the program?',
+            choices: ['Add an Engineer', 'Add an Intern', 'Quit the Program'],
+        }])
+    .then((answers) => {
+        // IF THEY CHOSE ENGINEER - CALL THE addEngineer
+        const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerNumber);
+
+        team.push(manager);
+
+        if (answers.chooseType === "Add an Engineer") {
+            addEngineer();
+        } else if (answers.chooseType === "Add an Intern") {
+            addIntern();
+        } else if (answers.chooseType === "Quit the Program") {
+            quitProgram();
+        }
+    })
